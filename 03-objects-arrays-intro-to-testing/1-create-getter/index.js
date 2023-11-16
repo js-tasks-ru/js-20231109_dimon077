8f6c733390371bea3e73aff74a6537e9af22ecbb
Dima Kushchenko <dimon077@yandex.ru>
2023-11-16 10:19:54 +0300

    task 03-1 | select value from object

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,19 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const pathArr = path.split('.');
 
+  return (obj) => {
+    if (Object.keys(obj).length == 0) {
+      return undefined;
+    }
+
+    let value = obj;
+
+    for (let i of pathArr) {
+      value = value[i];
+    }
+
+    return value;
+  };
 }
