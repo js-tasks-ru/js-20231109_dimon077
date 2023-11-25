beebe322afb6d8f79354e459cd2651f97b88805f
Dima Kushchenko <dimon077@yandex.ru>
2023-11-25 20:17:32 +0300

    task 5-1 | NotificationMessage component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,51 @@
 export default class NotificationMessage {
+  constructor(message = '', { duration = 0, type = 'success' } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.target = document.body;
 
+    this.element = this.createElement(this.createTemplate());
+  }
+
+  createElement = (template) => {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  createTemplate = () => {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">${this.message}</div>
+        </div>
+      </div>
+    `;
+  }
+
+  show = (target = this.target) => {
+    const notification = document.querySelector('.notification');
+
+    if (notification) {
+      notification.remove();
+    }
+
+    target.append(this.element);
+    this.removeOnTimer(this.duration);
+  }
+
+  removeOnTimer = (duration) => {
+    setTimeout(this.remove, duration);
+  }
+
+  remove = () => {
+    this.element.remove();
+  }
+
+  destroy = () => {
+    this.remove();
+  }
 }
