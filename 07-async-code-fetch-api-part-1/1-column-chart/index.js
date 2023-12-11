9ea843bf0fd25c75dc48c0f61cfdb852deba0bdb
Dima Kushchenko <dimon077@yandex.ru>
2023-12-11 16:35:37 +0300

    task 7-1 | columnhart v2

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -1,7 +1,43 @@
 import fetchJson from './utils/fetch-json.js';
+import ColumnChartV1 from '../../04-oop-basic-intro-to-dom/1-column-chart/index.js';
 
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ColumnChart {
+export default class ColumnChart extends ColumnChartV1 {
+  constructor(obj = {}) {
+    super(obj);
+    const { url, range = { from: new Date(), to: new Date() } } = obj;
 
+    this.url = new URL(url, BACKEND_URL);
+    this.range = range;
+    this.subElements = {
+      header: this.element.querySelector(`[data-element="header"]`),
+      body: this.element.querySelector(`[data-element="body"]`)
+    }
+
+    this.update(this.range.from, this.range.to);
+  }
+
+  update = async (from, to) => {
+    const url = new URL(this.url);
+    url.searchParams.set('from', from);
+    url.searchParams.set('to', to);
+
+    const data = await fetchJson(url);
+
+    this.data = Object.values(data);
+    this.render(this.data);
+
+    return data;
+  }
+
+  render = (data) => {
+    if (data.length === 0) {
+      this.showDataIsLoading();
+      return;
+    }
+
+    this.element.classList.remove('column-chart_loading');
+    this.subElements.body.innerHTML = this.createChartsTemplate(data);
+  }
 }
